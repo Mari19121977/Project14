f0fc721b16eda5dacba883511bfca2cfdf55df4b
Mari19121977 <ymi191219@gmail.com>
2021-07-21 19:58:20 +0300

    Add project files.

---
+++ Project14/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class red_book {
+
+};
+class animals : public red_book
+{
+public:
+	animals() {}
+	virtual void set(char* name) {
+
+	}
+	virtual void add() = 0;
+
+};
